6be9412044b764d3cefd229521ef5f104756b8ea
Hiro Ayettey <hiro.ayettey@gmail.com>
2020-04-30 17:47:41 -0400

    Initial Commit

---
+++ Women In Stem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Women In Stem
+//
+//  Created by Hiro Ayettey on 2020-04-30.
+//  Copyright © 2020 Hiro Ayettey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
